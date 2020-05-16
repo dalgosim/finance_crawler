55ccda5f834966efab409310e14671068e2c6ead
sangkwon <andy.sangkwon@gmail.com>
2020-05-17 05:11:15 +0900

    크롤러 틀 추가

---
+++ crawler/crawler.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from util import logger
+
+class Crawler:
+    
+    def __init__(self, delay=1):
+        self.logger = logger.APP_LOGGER
+        self.delay = delay
+
+    def crawl(self):
+        pass
+
+    def save(self):
+        pass
