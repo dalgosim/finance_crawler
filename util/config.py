1d7a3b1bfb08fae337d7cd0e0f97d5c3ebc75024
sangkwon <dalgosim@gmail.com>
2020-06-03 02:29:14 +0900

    계정설정 따로 불러오도록 변경

--- util/config.py
+++ util/config.py
@@ -7,7 +7,15 @@ CONFIG = DotMap()
 def load_config(run_type='test', path='config.json'):
     global CONFIG
 
+    AUTH_PATH = './auth/mysql_auth.json'
+
+    # load scheduler config
     with open(path) as f:
+        jdata = json.load(f)
+        CONFIG = DotMap(jdata)
+    
+    # load database config
+    with open(AUTH_PATH) as f:
         jdata = json.load(f)
         CONFIG = DotMap(jdata)
         CONFIG.DATABASE = CONFIG.REAL_DB if run_type.lower() == 'real' else CONFIG.DEV_DB
