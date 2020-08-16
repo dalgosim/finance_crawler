26530dc2fb67b9f8fd49d7486d53289038378ac3
sangkwon <dalgosim@gmail.com>
2020-08-16 22:33:58 +0900

    yahoo에서 수집 실패시 & 상장된 종목일 때만 네이버에서 수집

--- crawler/data_reader/price_crawler.py
+++ crawler/data_reader/price_crawler.py
@@ -18,6 +18,18 @@ class DataReaderCrawler(Crawler):
         self.price_column = ['date', 'cmp_cd', 'open', 'close', 'high', 'low', 'adj_close', 'volume']
         self.table = config.CONFIG.MYSQL_CONFIG.TABLES.PRICE_TABLE
         self.naver_price = naver_price_crawler.NaverPriceCrawler()
+        self.prev_cmp_cd = self.__get_prev_comp_list()
+
+    def __get_prev_comp_list(self):
+        query = f'''
+        SELECT cmp_cd FROM {self.table}
+        WHERE date=(SELECT MAX(date) FROM {self.table});
+        '''
+        cmp_df = self.mysql.select_dataframe(query, log='get_prev_comp_list')
+        cmp_cd_list = cmp_df['cmp_cd'].values.tolist()
+        self.logger.debug(f'cmp_cd_list : {len(cmp_cd_list)}, {cmp_cd_list[:5]}')
+        return cmp_cd_list
+
 
     def crawl(self, save=False):
         self.logger.debug(f'Price crawling start ({self.basis_date})')
@@ -27,6 +39,7 @@ class DataReaderCrawler(Crawler):
         limit = 5 if config.TEST_MODE else 0
         cmp_cd_list = common_sql.get_company_list(limit)['cmp_cd'].values
         for i, code in enumerate(cmp_cd_list):
+            _df = None
             if i%100==0:
                 self.logger.debug(f'price crawling... ({i}/{len(cmp_cd_list)})')
             try:
@@ -35,10 +48,16 @@ class DataReaderCrawler(Crawler):
                 # 상폐등으로 해당 기간에 데이터가 없을 때
                 continue
             except (KeyError, ChunkedEncodingError):
-                _df = self.naver_price.crawl(code[:6])
+                # yahoo에서 데이터가 일정하게 제공되지 않음,
+                # 데이터가 없고 이전에 크롤링된적 있으면 네이버에서 수집
+                if code in self.prev_cmp_cd:
+                    _df = self.naver_price.crawl(code[:6])
+            finally:
+                timer.random_sleep(min_delay=self.delay)
+            if _df is None:
+                continue
             _df['cmp_cd'] = code
             price_df = pd.concat([price_df, _df], sort=False)
-            timer.random_sleep(min_delay=self.delay)
         self.logger.debug(f'Price crawling complete')
 
         if save:
