d12cf1bcf55a62931b0982f4e78fb5f5ee4820f1
dalgosim <dalgosim@gmail.com>
2021-01-03 01:27:23 +0900

    종목 수집 api 변경

--- crawler/kind_krx/krx_crawler.py
+++ crawler/kind_krx/krx_crawler.py
@@ -1,49 +1,52 @@
 # -*- coding: utf-8 -*-
 import pandas as pd
+import FinanceDataReader as fdr
 
 from crawler import Crawler
 from dev_util.util import timer, logger, config
 
 
 class KRXCrawler(Crawler):
-    '''comp.fnguid.com에서 metric_studio 관련 정보 가져오기'''
-    STOCK_TYPE = {
-        'kospi': 'stockMkt',
-        'kosdaq': 'kosdaqMkt'
-    }
+    '''KRX에서 상장종목 정보 가져오기'''
+
     COL_DICT = {
-        '회사명':'cmp_nm_kor',
-        '종목코드':'cmp_cd',
-        '업종':'category',
-        '주요제품':'major_product',
-        '상장일':'regi_date',
-        '결산월':'settlement_month',
-        '대표자명':'representative',
-        '홈페이지':'website',
-        '지역':'location',
+        'Symbol':'cmp_cd',
+        'Market':'market',
+        'Name':'cmp_nm_kor',
+        'Sector':'sector',
+        'Industry':'industry', 
+        'ListingDate':'regi_date', # 상장일
+        'SettleMonth':'settlement_month', # 결산월
+        'Representative':'representative',
+        'HomePage':'website',
+        'Region':'location',
+    }
+    CODE_DICT = {
+        'KOSPI':'KS',
+        'KOSDAQ':'KQ',
+        'KONEX':'KX',
     }
 
     def __init__(self):
         super().__init__()
+        self.table = config.CONFIG.MYSQL_CONFIG.TABLES.COMPANY_LIST_TABLE
 
-    def _get_download_corplist(self, market_type='kospi'):
+    def _get_download_corplist(self, market_type='KRX'):
         self.logger.debug(f'{market_type} crawling start')
-        fmt = '{:06d}.KS' if market_type == 'kospi' else '{:06d}.KQ'
-        _market_type = self.STOCK_TYPE[market_type]
         
-        url = 'http://kind.krx.co.kr/corpgeneral/corpList.do?method=download'
-        url = url + f'&marketType={_market_type}'
-        _df = pd.read_html(url, header=0)[0]
+        df = fdr.StockListing(market_type) # KRX, KOSPI, KOSDAQ
+        df = df.rename(columns=self.COL_DICT)
         
-        _df.종목코드 = _df.종목코드.map(fmt.format)
+        # 파생상품은 코드가 73501BA2 형태로 되어있어서 제외
+        df = df[df.cmp_cd.str.len()==6]
+
+        df['cmp_cd'] = df.apply(lambda row: f"{row[0]}.{self.CODE_DICT.get(row[1], '')}", axis=1)
+
         self.logger.debug(f'{market_type} crawling finish')
-        return _df
+        return df
 
     def crawl(self, save=False):
-        kospi_df = self._get_download_corplist('kospi')
-        kosdaq_df = self._get_download_corplist('kosdaq')
-        comp_df = pd.concat([kospi_df, kosdaq_df])
-        comp_df = comp_df.rename(columns=self.COL_DICT)
+        comp_df = self._get_download_corplist()
 
         if save:
             self.save(comp_df)
@@ -52,9 +55,8 @@ class KRXCrawler(Crawler):
     def save(self, df):
         if df is not None:
             self.logger.debug(f'KRX save start')
-            table = config.CONFIG.MYSQL_CONFIG.TABLES.COMPANY_LIST_TABLE
-            df.to_csv(f'log/comp_{self.basis_date}.csv', mode='w')
-            self.mysql.insert_dataframe(df, table)
-            self.logger.debug(f'KRX save start')
+#             df.to_csv(f'log/comp_{self.basis_date}.csv', mode='w')
+            self.mysql.insert_dataframe(df, self.table)
+            self.logger.debug(f'KRX save complete')
         else:
             self.logger.debug(f'KRX save fail : DataFrame is empty!')
